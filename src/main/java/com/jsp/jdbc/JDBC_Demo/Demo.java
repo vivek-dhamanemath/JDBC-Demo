fec5f5f000c1517fd52721b8cf3bd43c16148630
Vivek Dhamanemath <vivekjdwork@gmail.com>
2025-01-18 11:16:46 +0530

    adding first project of JDBC

---
+++ src/main/java/com/jsp/jdbc/JDBC_Demo/Demo.java
@@ -0,0 +1,50 @@
+package com.jsp.jdbc.JDBC_Demo;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		try {
+
+			Driver driver = new com.mysql.cj.jdbc.Driver();
+			DriverManager.registerDriver(driver);
+
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb","root","FE321869");
+
+			Statement st1 =conn.createStatement();
+			
+			Statement st2 =conn.createStatement(); 
+			
+			
+			int rowInserted = st1.executeUpdate("insert into student values(103,'vivek')");
+			
+			int rowUpdated = st2.executeUpdate("update student set name='rohit' where id=101");
+
+			if(rowInserted>0)
+			{
+				System.out.println("Data inserted into DB");
+
+			}else {
+				System.out.println("Data not inserted into DB");
+			}
+			
+			if(rowUpdated > 0)
+			{
+				System.out.println("Data Updated");
+
+			}else {
+				System.out.println("Data not Updated");
+			}
+			conn.close();
+
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
